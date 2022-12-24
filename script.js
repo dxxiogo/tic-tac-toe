6215bc5628b75e9a09c35597fea8cb06bfb28fe0
Diogo Santana <diogosantanafreitasuna@gmail.com>
2022-12-24 18:06:33 -0300

    Resolvendo erros e adicionando funcinalidades

--- script.js
+++ script.js
@@ -26,14 +26,19 @@ const area8 = document.querySelector("#area-8");
 const area9 = document.querySelector("#area-9");
 
 function analyzeWinner (firstArea, secondArea, thirdArea) {
-
     if(firstArea.firstChild.className == secondArea.firstChild.className && firstArea.firstChild.className == thirdArea.firstChild.className){
         const scorePlayer1 = document.querySelector('#player01-score');
         const scorePlayer2 = document.querySelector('#player02-score');
-        const classElement = area1.firstChild.className;
+        const classElement = firstArea.firstChild.className;
+        const namePlayer1 = document.querySelector('#input-player01').value;
+        const namePlayer2 = document.querySelector('#input-player02').value;
         if(classElement == 'x-icon') {
+            alert(`${namePlayer1} ganhou a partida!`);
+            resartGame();
             scorePlayer1.innerText = Number(scorePlayer1.innerText) + 1;
         } else {
+            alert(`${namePlayer2} ganhou a partida!`);
+            resartGame();
             scorePlayer2.innerText = Number(scorePlayer2.innerText) + 1;
         }
     }   
@@ -52,32 +57,46 @@ gameAreas.forEach((area) => {
                 const newCircle = circleIcon.cloneNode(true);
                 classElement = 'x-icon';
                 chosenArea.append(newCircle);
+                numberOfMoves ++;
             } else {
                 const xIcon = document.querySelector('.x-icon');
                 const newX = xIcon.cloneNode(true);
                 classElement = 'circle-icon';
                 chosenArea.append(newX);
+                numberOfMoves++;
             } 
         }
         if(numberOfMoves >= 5){
             if(area1.childNodes.length > 0 && area5.childNodes.length > 0 && area9.childNodes.length > 0 ){
+                //Verificando transversal esquerda
                 analyzeWinner(area1, area5, area9);
-            } else if (area3.childNodes.length > 0 && area5.childNodes.length > 0 && area9.childNodes.length > 0 ){
-                analyzeWinner(area3, area5, area9);
-            } else if (area4.childNodes.length > 0 && area5.childNodes.length > 0 && area6.childNodes.length > 0 ){
+            }
+            if (area3.childNodes.length > 0 && area5.childNodes.length > 0 && area7.childNodes.length > 0 ){
+                //Verificando transversal direita
+                analyzeWinner(area3, area5, area7);
+            } 
+            if (area4.childNodes.length > 0 && area5.childNodes.length > 0 && area6.childNodes.length > 0 ){
                 analyzeWinner(area4, area5, area6);
-            } else if (area1.childNodes.length > 0 && area2.childNodes.length > 0 && area3.childNodes.length > 0 ){
+            } 
+            if (area1.childNodes.length > 0 && area2.childNodes.length > 0 && area3.childNodes.length > 0 ){
                 analyzeWinner(area1, area2, area3);
-            } else if (area7.childNodes.length > 0 && area8.childNodes.length > 0 && area9.childNodes.length > 0 ){
+            } 
+            if (area7.childNodes.length > 0 && area8.childNodes.length > 0 && area9.childNodes.length > 0 ){
                 analyzeWinner(area7, area8, area9);
-            } else if( area1.childNodes.length > 0 && area4.childNodes.length > 0 && area7.childNodes.length > 0 ){
+            } 
+            if( area1.childNodes.length > 0 && area4.childNodes.length > 0 && area7.childNodes.length > 0 ){
                 analyzeWinner(area1, area4, area7);
-            } else if(area2.childNodes.length > 0 && area5.childNodes.length > 0 && area8.childNodes.length > 0 ){
+            } 
+            if(area2.childNodes.length > 0 && area5.childNodes.length > 0 && area8.childNodes.length > 0 ){
                 analyzeWinner(area2, area5, area8);
-            } else if (area3.childNodes.length > 0 && area6.childNodes.length > 0 && area9.childNodes.length > 0 ){
+            } 
+            if (area3.childNodes.length > 0 && area6.childNodes.length > 0 && area9.childNodes.length > 0 ){
                 analyzeWinner(area3, area6, area9);
             }
-        }
+            if(numberOfMoves == 9) {
+                alert("Empate!");
+            }
+        } 
     });
 });
 
@@ -86,8 +105,10 @@ function resartGame () {
     console.log(res);
     if(res){
         gameAreas.forEach((area) => {
-            const elementArea = area.firstChild;
-            area.removeChild(elementArea);
+                const elementArea = area.firstChild;
+                if(elementArea != null){
+                    elementArea.remove();
+                }
         });
     }
 }
